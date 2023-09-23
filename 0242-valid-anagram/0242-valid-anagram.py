f067fa00168cce97cc97720a9b3e380af336f2b0
Pawel Borkar <36134699+pawelborkar@users.noreply.github.com>
2023-09-23 13:12:07 +0530

    Time: 53 ms (66.02%), Space: 16.8 MB (56.79%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        counter_s = collections.Counter(s)
-        counter_t = collections.Counter(t)
 
         # Check if the lengths are equal
         if len(s) != len(t):
             return False
         
-        # If equal then count the occurence of each character using counter
-
+        # Counts the occurence of each character in the given strings
+        counter_s = collections.Counter(s)
+        counter_t = collections.Counter(t)
+        
+        # Checks for exact occurences of a character in both the strings
         for i, char in enumerate(s):
             if counter_s.get(char) != counter_t.get(char):
                 return False
