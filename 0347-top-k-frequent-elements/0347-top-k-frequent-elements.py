38bd1112376e69ab842f42b84ea12bd6549992a0
Pawel Borkar <36134699+pawelborkar@users.noreply.github.com>
2023-10-07 09:48:16 +0530

    Time: 86 ms (97.3%), Space: 20.8 MB (92.75%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,9 +1,7 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
-        if k == 0:
-            return nums
+        # Since we're given the range of k in the form of [1, no. of unique elements] then don't have to check for nullables of k 
 
         count = collections.Counter(nums)
 
-        return heapq.nlargest(k, count.keys(), key= count.get)
+        return heapq.nlargest(k, count.keys(), key=count.get)
