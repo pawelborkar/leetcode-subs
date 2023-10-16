e44fbbc134f473cfd4528f39bd433bb8565471f9
Pawel Borkar <36134699+pawelborkar@users.noreply.github.com>
2023-10-16 16:37:08 +0530

    Time: 51 ms (31.26%), Space: 16.6 MB (53.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,25 +1,27 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        # Using Modified Binary Search
+	def search(self, nums: List[int], target: int) -> int:
 
-        left, right = 0, len(nums) - 1
+		# using binary search
+		left, right = 0, len(nums) - 1
 
-        while left <= right:
-            mid = (left + right) // 2
+		while left <= right:
+			mid = (left + right) // 2
 
-            # It will always return the result (if present)
-            if nums[mid] == target:
-                return mid
-            
-            # left sorted array
-            if nums[left] <= nums[mid]:
-                if target < nums[left] or target > nums[mid]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-            else:
-                if target > nums[right] or target < nums[mid]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-        return -1
+			if nums[mid] == target:
+				return mid
+
+
+			# left sorted array	
+			if nums[mid] >= nums[left]:
+				if target >= nums[left] and target < nums[mid]:
+					right = mid - 1
+				else:
+					left = mid + 1
+
+			else:
+				if target <= nums[right] and target > nums[mid]:
+					left = mid + 1
+				else:
+					right = mid - 1
+
+		return -1
